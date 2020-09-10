4d10d8204ebed16b1b040225b0de064e4101b54d
DCRichards <DCRichards@live.co.uk>
2020-09-10 18:43:09 +0100

    Added initial view test

---
+++ test/unit/views/Place.test.js
@@ -0,0 +1,50 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import { expect } from 'chai';
+import { spy } from 'sinon';
+import Place from '@/views/Place.vue';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe('Place.vue', () => {
+  const mockPlaceGetter = spy();
+  const mockGetAction = spy();
+  const store = new Vuex.Store({
+    modules: {
+      places: {
+        namespaced: true,
+        state: {
+          places: [],
+          loading: false,
+        },
+        getters: {
+          place: mockPlaceGetter,
+        },
+        actions: {
+          get: mockGetAction,
+        },
+      },
+    },
+  });
+
+  beforeEach(() => {
+    mockPlaceGetter.resetHistory();
+    mockGetAction.resetHistory();
+  });
+
+  it('Should load place from store on create', () => {
+    const $route = {
+      params: { id: '1022' },
+    };
+
+    shallowMount(Place, {
+      store,
+      localVue,
+      mocks: { $route },
+      stubs: ['router-link'],
+    });
+
+    expect(mockGetAction.firstCall.args[1]).to.eql({ id: $route.params.id });
+  });
+});
